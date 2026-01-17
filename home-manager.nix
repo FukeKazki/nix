80e56d289fd9cd76a6927959c16542fdedde3f62
FukeKazki <kazkichi0906@gmail.com>
2026-01-17 23:51:21 +0900

    feat: direnvのzsh連携を追加

--- home-manager.nix
+++ home-manager.nix
@@ -7,8 +7,10 @@
     programs = {
       direnv = {
         enable = true;
+        enableZshIntegration = true;
         nix-direnv.enable = true;
       };
+      zsh.enable = true;
     };
   };
 }
